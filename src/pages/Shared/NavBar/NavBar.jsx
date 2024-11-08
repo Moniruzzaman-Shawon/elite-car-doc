c12a4c10cf5b427350a79f471721379610c0a2d0
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-11-08 08:01:10 +0600

    banner & text added

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../../assets/logo2.svg'
+import logo2 from '../../../assets/logo2.svg'
 
 const NavBar = () => {
+    const navItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/'>About</Link></li>
+        <li><Link to='/'>Contact</Link></li>
+        <li><Link to='/'>Blog</Link></li>
+
+    </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 h-28 mb-4">
             <div className="navbar-start">
+
                 <div className="dropdown">
+
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+
+                    </div>
+
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {navItems}
+                    </ul>
+                </div>
+                <div><img src={logo2} alt="" /></div>
+                {/* New Dropdown with Menu Icon */}
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost flex items-center">
+                        {/* Hamburger Icon */}
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
@@ -18,50 +41,30 @@ const NavBar = () => {
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                                 strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16" />
+                                d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
-
+                        {/* Text can be added after the icon */}
+                        <span className="ml-2"></span>
                     </div>
-
-
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navItems}
                     </ul>
                 </div>
 
-
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-
+                {/* daisyUI button */}
+                <a className="btn btn-ghost text-xl">Elite Car Doctor</a>
             </div>
+
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navItems}
                 </ul>
             </div>
 
             <div className="navbar-end">
-                <a className="btn">Get Started</a>
+                <button className="btn btn-outline btn-info">APPOINTMENT</button>
             </div>
         </div>
     );
