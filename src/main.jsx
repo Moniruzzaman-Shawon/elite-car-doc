3ea6e70561f394272b639ecba310e2f04b2edd9b
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-11-08 06:53:36 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routes/Routes.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <div className='max-w-7xl mx-auto'>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  </div>
+)
